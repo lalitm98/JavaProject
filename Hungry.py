28e3874f689ceba71e03cb1147285c01189616a8
lalitm98 <malilalit998@gmail.com>
2022-05-26 15:33:58 +0530

    water

--- Hungry.py
+++ Hungry.py
@@ -1,3 +1,9 @@
-print("i'm Hungry")
-print("I want pizza")
-print("I want burger")
+hungry = input("are you hungry?")
+if hungry=="yes"
+    print("eat samosa")
+    print("eat pizza")
+    print("eat burger")
+else:
+    main2=input("are yoy main2?")
+    if main2=="yes"
+    print("drink water")
