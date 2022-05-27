4d69b68dcefe888e1a259347f6846fa9ee4c43b6
lalitm98 <malilalit998@gmail.com>
2022-05-27 11:53:46 +0530

    changes commited

--- Hungry.py
+++ Hungry.py
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
+
 hungry = input("are you hungry?")
 if hungry=="yes"
     print("eat samosa")
     print("eat pizza")
     print("eat burger")
-else:
-    main2=input("are yoy main2?")
-    if main2=="yes"
-    print("drink water")
-    print("drink soda")
-=======
-print("i'm Hungry")
-print("I want pizza")
-print("I want burger")
-print("i want fries")
->>>>>>> main2
